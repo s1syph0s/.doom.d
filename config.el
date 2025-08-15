86afcc974095846de8abda1d41c64f95fbdf3ddd
Pasha Fistanto <pasha@fstn.top>
2025-08-15 18:39:07 +0200

    cc-mode: same level indentation in namespace

--- config.el
+++ config.el
@@ -228,5 +228,38 @@
                                     token_tree token_tree_pattern
                                     token_repetition)))
 
+;; CC Mode indentation
+(c-add-style
+ "my-style" '((c-comment-only-line-offset . 0)
+              (c-hanging-braces-alist (brace-list-open)
+                                      (brace-entry-open)
+                                      (substatement-open after)
+                                      (block-close . c-snug-do-while)
+                                      (arglist-cont-nonempty))
+              (c-cleanup-list brace-else-brace)
+              (c-offsets-alist
+               (knr-argdecl-intro . 0)
+               (substatement-open . 0)
+               (substatement-label . 0)
+               (statement-cont . +)
+               (case-label . +)
+               ;; align args with open brace OR don't indent at all (if open
+               ;; brace is at eolp and close brace is after arg with no trailing
+               ;; comma)
+               (brace-list-intro . 0)
+               (brace-list-close . -)
+               (arglist-intro . +)
+               (arglist-close +cc-lineup-arglist-close 0)
+               ;; don't over-indent lambda blocks
+               (inline-open . 0)
+               (inlambda . 0)
+               ;; indent access keywords +1 level, and properties beneath them
+               ;; another level
+               (access-label . -)
+               (inclass +cc-c++-lineup-inclass +)
+               (label . 0)
+               (innamespace . 0))))
+(after! cc-mode
+  (setf (alist-get 'other c-default-style) "my-style"))
 
 (remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
