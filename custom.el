833ba8ae8c55818ba1aec878b7a797c1ec2921cd
Pasha Fistanto <fistanto@ibr.cs.tu-bs.de>
2026-02-05 09:17:54 +0100

    safe dir

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default)))
+   '("0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default))
+ '(safe-local-variable-directories
+   '("/home/fistanto/src/co/smalloc/" "/home/fistanto/.doom.d/" "~/.emacs.d/")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
