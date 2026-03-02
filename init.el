3460bf17b49342ba58abfcc03f037825d4c17219
Pasha Fistanto <pasha@fstn.top>
2026-03-02 11:59:36 +0100

    add dtrt indent

--- init.el
+++ init.el
@@ -53,6 +53,7 @@
        ;;zen               ; distraction-free coding or writing
 
        :editor
+       (whitespace +guess)
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
